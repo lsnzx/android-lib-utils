4042cdc369b1996bc8013e52475fab3e65676a85
lzq <761048783@qq.com>
2023-06-24 10:06:44 +0800

    v1.0.0

--- android-lib-utils/build.gradle.kts
+++ android-lib-utils/build.gradle.kts
@@ -36,31 +36,17 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.lsn.lib"
                 artifactId = "utils"
-                version = "1.2.2"
+                version = "1.0.0"
 //                from(components["java"])
                 afterEvaluate { artifact(tasks.getByName("bundleReleaseAar")) }
             }
         }
     }
-
 }
 
 
 
 dependencies {
-
-//    implementation("com.google.code.gson:gson:2.9.0")
-//    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
-//
-//    implementation("com.google.android.material:material:1.8.0")
-//    implementation("androidx.appcompat:appcompat:1.6.1")
-//    implementation("androidx.core:core-ktx:1.9.0")
-//    implementation("androidx.activity:activity-ktx:1.6.1")
-//    implementation("androidx.fragment:fragment-ktx:1.5.5")
-//    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-//    implementation("androidx.recyclerview:recyclerview:1.2.0")
-//    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.0")
 
     implementation(libs.gson)
     implementation(libs.okhttp3.logging.interceptor)
